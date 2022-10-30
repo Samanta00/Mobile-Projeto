929aef7f50ddfc0c08f84c6b7edaa9ce40e08e37
Ellen Samanta <ellen.samanta@outlook.com>
2022-10-30 16:15:40 -0300

    segundo-commit

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//           <NavigationContainer>
+//           <Stack.Navigator initialRouteName="Home">
+//             <Stack.Screen name="Home" component={HomeScreen} />
+//             <Stack.Screen name="Details" component={DetailsScreen} />
+//           </Stack.Navigator>
+//         </NavigationContainer>
+//     </View>
+//   );
+// }
+
+// ;
---
+++ src/menu.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+
+export default function Menu(){
+
+    return(
+        <View>
+            <StatusBar style="auto"/>
+            <Text>Olá</Text>
+        </View>
+    )
+}
+
+
+module.exports=Menu
